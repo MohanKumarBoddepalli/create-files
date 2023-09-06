66468c2bc9a86ac42a067f722e2e9959d952ba3e
mohanu998 <mohankumarboddepalli@outlook.com>
2023-09-06 23:31:50 +0530

    layout change

--- src/ZipComponent/index.tsx
+++ src/ZipComponent/index.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import JSZip from 'jszip';
-import { Button, Modal, Form, Input, Select, Row, Col } from 'antd';
+import { Button, Modal, Form, Input, Select, Row, Col, Layout } from 'antd';
 import './component.css';
 import { PlusOutlined } from '@ant-design/icons';
 import { Divider, Space } from 'antd';
-
+const { Header, Content, Footer, Sider } = Layout;
 const { Option } = Select;
 const FormItem = Form.Item;
 let index = 0;
@@ -32,7 +32,7 @@ const ZipDownloadComponent = () => {
 
 
   const onFinish = (values: any) => {
-    const updatedValues : any = {};
+    const updatedValues: any = {};
     for (let i = 0; i < textAreas.length; i++) {
       updatedValues[textAreas[i]] = values[textAreas[i]] !== '' ? values[textAreas[i]].split('\n') : [];
     }
@@ -68,11 +68,11 @@ const ZipDownloadComponent = () => {
   const handleOk = () => {
     // Perform any logic you want with the input value
     console.log('Input value:', inputValue);
-    if(inputValue) {
+    if (inputValue) {
       const pushvalue = [...textAreas, inputValue]
       setTextAreas(pushvalue);
     }
-  
+
 
     // Close the modal
     setVisible(false);
@@ -90,129 +90,140 @@ const ZipDownloadComponent = () => {
 
 
   return (
-    <div className='divs'>
-      <Form
-        name="nest-messages"
-        onFinish={onFinish}
-        style={{ maxWidth: '100%' }}
-        layout="vertical"
-        labelAlign='left'
-        initialValues={{ startingNo: 0, noOfFiles: 0 }}
-      >
-        <Row className='flex-container'>
-          <Col span={11}>
-            <Form.Item name={['foldername']} label="Folder Name" rules={[{ required: true }]}>
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={11} offset={2}>
-            <Form.Item name={['filename']} label="FileName" rules={[{ required: true }]}>
-              <Input />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={11}>
-            <Form.Item name={['startingNo']} label="File Starting no">
-              <Input type="number" ></Input>
-            </Form.Item>
-          </Col>
-          <Col span={11} offset={2}>
-            <Form.Item name={['filetype']} label="File Type" rules={[{ required: true }]}>
-              <Select
-                style={{ width: '100%' }}
-                placeholder="Custom Dropdown"
-                dropdownRender={(menu) => (
-                  <>
-                    {menu}
-                    <Divider style={{ margin: '8px 0' }} />
-                    <Space style={{ padding: '0 8px 4px' }}>
-                      <Input
-                        placeholder="Please enter item"
-                        value={name}
-                        onChange={onNameChange}
-                      />
-                      <Button type="text" icon={<PlusOutlined />} onClick={addItem}>
-                        Add item
-                      </Button>
-                    </Space>
-                  </>
-                )}
-              >
-                {items.map((item) => (
-                  <Select.Option key={item} value={item}>
-                    {item}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={11}>
-            <Form.Item name={['noOfFiles']} label="No of files to create">
-              <Input type="number" ></Input>
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row >
-          <Col span={24}>
-            <Form.Item name={['componentValue']} label="File input text">
-              <Input.TextArea
-                autoSize={{ minRows: 3, maxRows: 3 }}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        <FormItem>
-          {textAreas.length > 0 ? <div className="dynamic-flex-container">
-            {textAreas.map((textArea, index) => (
-              <div className='flex-item'>
-                <Form.Item key={index}
-                  name={[`${textArea}`]}
-                  label={textArea}>
+    <Layout className='layout'>
+      <Header className='header' style={{textAlign:'left'}}>
+        <h2>Create Files</h2>
+      </Header>
+      <Content className='content'>
+        <div className='divs'>
+          <Form
+            name="nest-messages"
+            onFinish={onFinish}
+            style={{ maxWidth: '100%' }}
+            layout="vertical"
+            labelAlign='left'
+            initialValues={{ startingNo: 0, noOfFiles: 0 }}
+          >
+            <Row className='flex-container'>
+              <Col span={11}>
+                <Form.Item name={['foldername']} label="Folder Name" rules={[{ required: true }]}>
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col span={11} offset={2}>
+                <Form.Item name={['filename']} label="FileName" rules={[{ required: true }]}>
+                  <Input />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={11}>
+                <Form.Item name={['startingNo']} label="File Starting no">
+                  <Input type="number" ></Input>
+                </Form.Item>
+              </Col>
+              <Col span={11} offset={2}>
+                <Form.Item name={['filetype']} label="File Type" rules={[{ required: true }]}>
+                  <Select
+                    style={{ width: '100%' }}
+                    placeholder="Custom Dropdown"
+                    dropdownRender={(menu) => (
+                      <>
+                        {menu}
+                        <Divider style={{ margin: '8px 0' }} />
+                        <Space style={{ padding: '0 8px 4px' }}>
+                          <Input
+                            placeholder="Please enter item"
+                            value={name}
+                            onChange={onNameChange}
+                          />
+                          <Button type="text" icon={<PlusOutlined />} onClick={addItem}>
+                            Add item
+                          </Button>
+                        </Space>
+                      </>
+                    )}
+                  >
+                    {items.map((item) => (
+                      <Select.Option key={item} value={item}>
+                        {item}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={11}>
+                <Form.Item name={['noOfFiles']} label="No of files to create">
+                  <Input type="number" ></Input>
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row >
+              <Col span={24}>
+                <Form.Item name={['componentValue']} label="File input text">
                   <Input.TextArea
-                    className="scrollable-textarea"
                     autoSize={{ minRows: 3, maxRows: 3 }}
-                    placeholder={`Enter Text Area ${index + 1}`}
                   />
                 </Form.Item>
-              </div>
-            ))}
-          </div> : <></>}
-        </FormItem>
+              </Col>
+            </Row>
+            <FormItem>
+              {textAreas.length > 0 ? <div className="dynamic-flex-container">
+                {textAreas.map((textArea, index) => (
+                  <div className='flex-item'>
+                    <Form.Item key={index}
+                      name={[`${textArea}`]}
+                      label={textArea}>
+                      <Input.TextArea
+                        className="scrollable-textarea"
+                        autoSize={{ minRows: 3, maxRows: 3 }}
+                        placeholder={`Enter Text Area ${index + 1}`}
+                      />
+                    </Form.Item>
+                  </div>
+                ))}
+              </div> : <></>}
+            </FormItem>
 
-        <Row>
-          <Col>
-            <div>
-              <Button type="primary" onClick={showModal}>
-                Add key value
-              </Button>
-              <Modal
-                title="Enter key you want to add"
-                visible={visible}
-                onOk={handleOk}
-                onCancel={handleCancel}
-              >
-                <Input
-                  placeholder="Name of parameter"
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  required
-                />
-              </Modal>
-            </div>
-          </Col>
-          <Col>
-            <Form.Item wrapperCol={{ offset: 8 }}>
-              <Button type="primary" htmlType="submit">
-                create & download
-              </Button>
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
-    </div>
+            <Row>
+              <Col>
+                <div>
+                  <Button type="primary" onClick={showModal}>
+                    Add key value
+                  </Button>
+                  <Modal
+                    title="Enter key you want to add"
+                    visible={visible}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
+                  >
+                    <Input
+                      placeholder="Name of parameter"
+                      value={inputValue}
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </Modal>
+                </div>
+              </Col>
+              <Col>
+                <Form.Item wrapperCol={{ offset: 8 }}>
+                  <Button type="primary" htmlType="submit">
+                    create & download
+                  </Button>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      </Content>
+      <Footer className='footer' style={{ textAlign: 'right' }}>
+      ©2023 Created by Mohan kumar
+      </Footer>
+    </Layout>
+
   );
 };
 
